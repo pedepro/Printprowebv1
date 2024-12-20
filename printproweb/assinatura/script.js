979cf56733651aa0452056ef763b436094e10773
pedepro <administrativo@pedepro.com.br>
2024-12-20 13:11:02 -0300

    Add files via upload

--- printproweb/assinatura/script.js
+++ printproweb/assinatura/script.js
@@ -1,13 +1,29 @@
 // Função para assinar o plano Start
 function subscribe(plan) {
     if (plan === 'start') {
-        alert('Você assinou o plano Start!');
-        // Aqui pode-se adicionar lógica de navegação ou integração com sistema de pagamento
+        // Mensagem de confirmação
+        alert('Você será redirecionado para a página de formulário para completar sua assinatura do plano Start.');
+
+        // Navegação para a pasta "formulario"
+        window.location.href = '../formulario/';
     }
 }
 
+
 // Função para contato com a equipe de vendas no plano My Software
 function contactSales() {
-    alert('Você será redirecionado para nossa equipe de vendas!');
-    // Aqui pode-se adicionar lógica de redirecionamento ou integração com o sistema de atendimento
+    const phoneNumber = '5549920009371'; // Número de telefone no formato internacional
+    const message = 'Olá, quero assinar o plano My Software Personalizado PrintPro'; // Mensagem para enviar
+
+    // URL para redirecionar ao WhatsApp
+    const whatsappURL = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+
+    // Redirecionar o usuário para o WhatsApp
+    window.location.href = whatsappURL;
 }
+
+
+document.getElementById('register-link').addEventListener('click', function() {
+    // Redirecionar para a pasta formulário
+    window.location.href = '../login';
+});
